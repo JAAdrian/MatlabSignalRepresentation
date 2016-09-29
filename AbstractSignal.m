04274edaeb03aa8e054d8b27ee8c592da062b806
Jens-Alrik Adrian <jens-alrik.adrian@jade-hs.de>
2016-09-29 18:50:13 +0200

    Create the abstract signal class

---
+++ AbstractSignal.m
@@ -0,0 +1,49 @@
+classdef AbstractSignal < handle
+%ABSTRACTSIGNAL <purpose in one line!>
+% -------------------------------------------------------------------------
+% <Detailed description of the function>
+%
+% AbstractSignal Properties:
+%	propA - <description>
+%	propB - <description>
+%
+% AbstractSignal Methods:
+%	doThis - <description>
+%	doThat - <description>
+%
+% Author :  J.-A. Adrian (JA) <jens-alrik.adrian AT jade-hs.de>
+% Date   :  29-Sep-2016 17:35:47
+%
+
+% History:  v0.1   initial version, 29-Sep-2016 (JA)
+%
+
+
+properties (Access = public)
+    Signal;
+    SampleRate;
+end
+
+properties (Access = public, Abstract)
+    NumChannels;
+    Duration;
+    NumSamples;
+end
+
+
+
+methods (Abstract)
+    [hf, ha] = plot(self);
+end
+
+methods
+    
+    
+    
+end
+
+
+end
+
+
+% End of file: AbstractSignal.m
